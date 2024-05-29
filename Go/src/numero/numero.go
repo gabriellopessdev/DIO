069a77cc95b3e18edf7fe99a642d9641af8f2da1
Gabriellopessdev <gabriellopessdev@gmail.com>
2024-05-29 10:12:22 -0300

    Curso Go

---
+++ Go/src/numero/numero.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+//Variaveis numeros:
+//unit = numero inteiro
+//int = numero real
+
+func main() {
+	fmt.Println("1 + 1 =", 1 + 1)
+}
---
+++ Go/src/string/string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello Word!!!"))//mostrar quantidade de caracteres
+	fmt.Println("Hello world!!!" [2])//Buscar a posição
+	fmt.Println("Hello" + "World!!!")//Concatenar as Strings
+}
